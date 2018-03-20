1131fa0e97a04ad400efdf7bd285944c709989f6
Joel Dean <jdeanjj1000@gmail.com>
2018-03-19 20:52:23 -0500

    removed abstract so gson doesnt fail to instantiate class

--- cobinhood/src/main/java/api/cobinhood/models/CobinResponse.java
+++ cobinhood/src/main/java/api/cobinhood/models/CobinResponse.java
@@ -5,10 +5,10 @@ package api.cobinhood.models;
  * Created by joel on 3/19/18.
  */
 
-public abstract class CobinResponse<T> {
+public class CobinResponse<T> {
+
     private T result;
     private boolean success;
-
 
     public T getResult() {
         return result;
